e6ea907e71f2b816a8ba1af8436f5a606735e50c
lizard-kim <rlaxodud1122@gmail.com>
2020-05-13 03:43:24 +0900

    [#3]Add idea model

--- board/admin.py
+++ board/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Idea
 
 admin.site.register(User)
+admin.site.register(Idea)
+
--- board/models.py
+++ board/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+
+class Idea(models.Model):
+    title = models.CharField(
+        max_length=50,
+    )
+    date = models.DateField(
+        auto_now=True, 
+        auto_now_add=False
+    )
+    contents = models.TextField(
+        max_length=500,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.title
+
 class User(models.Model):
     name = models.CharField(
         max_length=20,
@@ -34,16 +51,12 @@ class User(models.Model):
         default="user"
         #user leader judge admin
     )
+    idea = models.ForeignKey(
+        Idea, 
+        null=True, 
+        blank=True,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
-
-'''
-이름
-생년월일
-연락처
-재학중인 학교
-이메일
-주소
-소속팀
-'''
