a9a1724b930cf22f69a036370f026a8396d450c2
lizard-kim <rlaxodud1122@gmail.com>
2020-05-12 14:24:07 +0900

    [#3] Add user model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(
+        max_length=20,
+        default="anonymous"
+    )
+    birthday = models.DateField(
+        auto_now=False, 
+        auto_now_add=False
+    )
+    phonenumber = models.CharField( 
+        max_length=20,
+        default="no number"
+    )
+    school = models.CharField(
+        max_length=20,
+        default = "no school"
+    )
+    email = models.CharField(
+        max_length=50,
+        default = "test@gmail.com"
+    )
+    address = models.CharField(
+        max_length=60,
+        default = "address"
+    )
+    team = models.CharField(
+        max_length=50,
+        default = "team"
+    )
+    auth = models.CharField(
+        max_length=10,
+        default="user"
+        #user leader judge admin
+    )
+
+    def __str__(self):
+        return self.name
+
+'''
+이름
+생년월일
+연락처
+재학중인 학교
+이메일
+주소
+소속팀
+'''
