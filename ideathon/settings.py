bdcf7e2b0a46a27dfaec574117ff506afa29725e
lizard-kim <rlaxodud1122@gmail.com>
2020-05-13 09:44:38 +0900

    for deploy

--- ideathon/settings.py
+++ ideathon/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!uv^s*21%$08jykhb%tnz+6h&$at+ufz$w#lvy840)0@g!5$v&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
