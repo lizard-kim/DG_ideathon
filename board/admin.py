14a4c5b7eef25f037b1d214c6b57d8339de3e807
lizard-kim <rlaxodud1122@gmail.com>
2020-05-15 20:47:13 +0900

    Add admin edu

--- board/admin.py
+++ board/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Idea, Notice, Recruit, Qna
+from .models import Profile, Idea, Notice, Recruit, Qna, Edu
 
 admin.site.register(Profile)
 admin.site.register(Idea)
 admin.site.register(Notice)
 admin.site.register(Recruit)
 admin.site.register(Qna)
+admin.site.register(Edu)
